e3b801025783a6d37074dcfef09dacd71eaebff5
ubiraven <admin@Mac-Admin.local>
2017-10-16 00:03:25 +0400

    First commit interface mostly

--- AtlanTeam Test/FirstViewController.swift
+++ AtlanTeam Test/FirstViewController.swift
@@ -9,9 +9,36 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var cardThreeStack: UIStackView!
+    @IBAction func userSelection(_ sender: Any) {
+        let selectedButton = sender as! UIButton
+        if selectedButton.isSelected == true {
+            UIView.animate(withDuration: 0.2) {
+                selectedButton.isSelected = false
+                self.cardThreeStack.arrangedSubviews[0].isHidden = false
+                self.cardThreeStack.arrangedSubviews[2].isHidden = false
+                self.cardThreeStack.arrangedSubviews[3].isHidden = false
+                self.cardThreeStack.arrangedSubviews[4].isHidden = false
+                (self.cardThreeStack.arrangedSubviews[5] as! UITextView).text = ""
+                self.cardThreeStack.arrangedSubviews[5].isHidden = true
+            }
+        } else {
+            UIView.animate(withDuration: 0.2) {
+                (self.cardThreeStack.arrangedSubviews[1] as! UIButton).isSelected = true
+                self.cardThreeStack.arrangedSubviews[0].isHidden = true
+                self.cardThreeStack.arrangedSubviews[2].isHidden = true
+                self.cardThreeStack.arrangedSubviews[3].isHidden = true
+                self.cardThreeStack.arrangedSubviews[4].isHidden = true
+                (self.cardThreeStack.arrangedSubviews[5] as! UITextView).text = "efefefefefefefefefefefefefefefefefefefefefefefefefefefefefefefefefefefefnvnvvcxbvnxbvsjfeopfe"
+                self.cardThreeStack.arrangedSubviews[5].isHidden = false
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.cardThreeStack.arrangedSubviews[5].isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
