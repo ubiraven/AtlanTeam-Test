e6beed3e2eb0c3c432fcce8ef3e6a16885207696
ubiraven <admin@Mac-Admin.local>
2017-10-15 18:58:02 +0400

    Initial Commit

---
+++ AtlanTeam Test/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AtlanTeam Test
+//
+//  Created by Admin on 15.10.17.
+//  Copyright © 2017 Sergey Artemyev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
