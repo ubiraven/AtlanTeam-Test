2da58a2a39151bf7e73a85cafd6fa8e9fd806764
ubiraven <admin@Mac-Admin.local>
2017-10-22 16:42:24 +0400

    almost done

--- AtlanTeam Test/FirstViewController.swift
+++ AtlanTeam Test/FirstViewController.swift
@@ -11,7 +11,7 @@ import SDWebImage
 import AFNetworking
 
 enum FieldNames: String {
-    case userId, id, title, body, postId, name, email, completed, username, phone, website
+    case userId, id, title, body, postId, name, email, completed, username, phone, website, address, street, suite, city, zipcode, geo, lat, lng, company, catchPhrase, bs, url
 }
 extension NSDictionary {
     subscript(_ field: FieldNames) -> Any? {
@@ -23,8 +23,11 @@ extension NSDictionary {
         while let object = result.object(forKey: fields[i].rawValue) as? NSDictionary {
             result = object
             i += 1
+            if i >= fields.count  {
+                break
+            }
         }
-        return result.object(forKey: fields[i].rawValue)
+        return result
     }
 }
 
@@ -34,7 +37,7 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         case posts = "https://jsonplaceholder.typicode.com/posts"
         case comments = "https://jsonplaceholder.typicode.com/comments"
         case users = "https://jsonplaceholder.typicode.com/users"
-        case photo = "https://jsonplaceholder.typicode.com/photos"
+        case photo = "https://jsonplaceholder.typicode.com/photos/3"
         case todos = "https://jsonplaceholder.typicode.com/todos"
     }
     enum Section: Int {
@@ -57,6 +60,13 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
     let requestOperationManager: AFHTTPRequestOperationManager = AFHTTPRequestOperationManager.init(
         baseURL: URL.init(string: "https://jsonplaceholder.typicode.com"))
     
+    @IBAction func clearKeyboard(_ sender: Any) {
+        for field in fields {
+            if field.isFirstResponder == true {
+                field.resignFirstResponder()
+            }
+        }
+    }
     @IBAction func userSelection(_ sender: Any) {
         let selectedButton = sender as! UIButton
         if selectedButton.isSelected == true {
@@ -78,13 +88,31 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
                     }
                 }
                 let tag = selectedButton.tag % 100 - 1
-                let text = self.composeTextFrom(self.data[3]![tag], keysOrder: FieldNames.id.rawValue,
-                                                                               FieldNames.name.rawValue,
-                                                                               FieldNames.username.rawValue,
-                                                                               FieldNames.email.rawValue,
-                                                                               //FieldNames.id.rawValue
-                                                                               FieldNames.phone.rawValue,
-                                                                               FieldNames.website.rawValue)
+                let data = self.data[3]![tag]
+                let text = self.composeTextFrom(data, keysOrder:
+                                  FieldNames.id.rawValue,
+                                  FieldNames.name.rawValue,
+                                  FieldNames.username.rawValue,
+                                  FieldNames.email.rawValue) + "\n" +
+                                  FieldNames.address.rawValue + ":\n" +
+                           self.composeTextFrom(data[FieldNames.address] as! NSDictionary, keysOrder:
+                                  FieldNames.street.rawValue,
+                                  FieldNames.suite.rawValue,
+                                  FieldNames.city.rawValue,
+                                  FieldNames.zipcode.rawValue) + "\n" +
+                                  FieldNames.geo.rawValue + ":\n" +
+                           self.composeTextFrom(data[FieldNames.address,FieldNames.geo] as! NSDictionary, keysOrder:
+                                  FieldNames.lat.rawValue,
+                                  FieldNames.lng.rawValue) + "\n" +
+                           self.composeTextFrom(data, keysOrder:
+                                  FieldNames.phone.rawValue,
+                                  FieldNames.website.rawValue) + "\n" +
+                                  FieldNames.company.rawValue + ":\n" +
+                           self.composeTextFrom(data[FieldNames.company] as! NSDictionary, keysOrder:
+                                  FieldNames.name.rawValue,
+                                  FieldNames.catchPhrase.rawValue,
+                                  FieldNames.bs.rawValue)
+                //let text = String.init(describing: data)
                 (self.cardThreeStack.arrangedSubviews[5] as! UITextView).text = text
                 self.cardThreeStack.arrangedSubviews[5].isHidden = false
                 self.cardThreeStack.distribution = .fill
@@ -101,25 +129,24 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         
         let usersURL = BaseURL.users.rawValue
         requestOperationManager.get(usersURL, parameters: nil, success: { (operation, responseObject) in
-            print(responseObject)
+            //print(responseObject)
             self.updateDataForSection(.users, withElement: responseObject as! Array<NSDictionary>)
         }) { (operation, error) in
             
         }
-        
         let toDosURL = BaseURL.todos.rawValue + "/" + String(arc4random_uniform(200) + 1)
         requestOperationManager.get(toDosURL, parameters: nil, success: { (operation, responseObject) in
-            print(responseObject)
+            //print(responseObject)
             self.updateDataForSection(.todos, withElement: Array.init(arrayLiteral: responseObject as! NSDictionary))
         }) { (operation, error) in
             
         }
-        
-        
-        let imageURL = URL.init(string: "https://placehold.it/600/24f355")
-        imageView.sd_setImageWithPreviousCachedImage(with: imageURL, placeholderImage: nil, options: SDWebImageOptions.lowPriority, progress: { (receivedSize: Int, expectedSize: Int) in
-        }, completed: { (image: UIImage?, error: Error?, cacheType: SDImageCacheType, imageURL: URL?) in
-        })
+        let photoURL = BaseURL.photo.rawValue
+        requestOperationManager.get(photoURL, parameters: nil, success: { (operation, responseObject) in
+            self.updateDataForSection(.photo, withElement: Array.init(arrayLiteral: responseObject as! NSDictionary))
+        }) { (opration, error) in
+            
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -171,6 +198,13 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
                     break
                 }
             }
+        case 4:
+            let photoURL = data[4]![0][FieldNames.url] as! String
+            let _photoURL = photoURL.replacingOccurrences(of: "http", with: "https")
+            let url = URL.init(string: _photoURL)
+            imageView.sd_setImageWithPreviousCachedImage(with: url, placeholderImage: nil, options: SDWebImageOptions.lowPriority, progress: { (receivedSize: Int, expectedSize: Int) in
+            }, completed: { (image: UIImage?, error: Error?, cacheType: SDImageCacheType, imageURL: URL?) in
+            })
         default:
             break
         }
@@ -212,6 +246,46 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
             } else {
                 return true
             }
+        } else {
+            return false
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text != nil {
+            var number = Int(textField.text!)
+            if number == 0 {
+                textField.text = "1"
+                number = 1
+            }
+            switch textField.tag {
+            case 101:
+                if number! > 100 {
+                    textField.text = "100"
+                    number = 100
+                }
+                let postsURL = BaseURL.posts.rawValue + "/" + textField.text!
+                requestOperationManager.get(postsURL, parameters: nil, success: { (operation, responseObject) in
+                    //print(responseObject)
+                    self.updateDataForSection(.posts, withElement: Array.init(arrayLiteral: responseObject as! NSDictionary))
+                }, failure: { (operation, error) in
+                    
+                })
+            case 201:
+                if number! > 500 {
+                    textField.text = "500"
+                    number = 500
+                }
+                let commentsURL = BaseURL.comments.rawValue + "/" + textField.text!
+                requestOperationManager.get(commentsURL, parameters: nil, success: { (operation, responseObject) in
+                    //print(responseObject)
+                    self.updateDataForSection(.comments, withElement: Array.init(arrayLiteral: responseObject as! NSDictionary))
+                }, failure: { (operation, error) in
+                    
+                })
+            default:
+                break
+            }
+            return true
         } else {
             return false
         }
